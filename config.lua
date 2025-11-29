deea1e66089d8c8e28edd4eadc94a542f5de4d8a
Chaosmann09 <56198093+chaosmann09@users.noreply.github.com>
2025-11-29 10:24:55 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,26 @@
+Config = {}
+
+Config.Farm = {
+    Type = {
+        {Label = "Kartoffeln", Value = "potato", BlipLabel = "Kartoffelfeld", Anim = "world_human_gardener_plant",BlipNumber = 208, Colour= 0, Time = 35000, Count = 6, x = 1956.4, y = 4797.42, z = 43.62},
+        {Label = "Metall", Value = "metall", BlipLabel = "Metallfabrik", Anim = "world_human_gardener_plant",BlipNumber = 618, Colour= 0,Time = 35000, Count = 6, x = 1088.1544, y = -2001.6482, z = 30.8798}, 
+        {Label = "Orangen", Value = "orage", BlipLabel = "Orangen", Anim = "world_human_gardener_plant",BlipNumber = 238, Colour= 47, Time = 2, Count = 6, x = 1564.7839, y = 2193.1218, z = 79.0672},                                                                                                                                                                                           
+
+    }
+}
+
+Config.Shop = {
+    Pos = {
+        {x = 386.06, y = -326.16, z = 46.87, a = 160.01, BlipLabel = "Händler"}
+    }
+}
+
+Config.Waffenverarbeiter = {
+    Pos = {
+        {x = 576.1370, y = -3124.0588, z = 18.7686, a = 88.2640}
+    }
+}
+
+Config.Moneywash = {
+    {-813.47, 179.41, 72.16}
+}
